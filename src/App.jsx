f0257e2b59b848964f3d38d4cc0d4ca8b60ca160
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-04-06 04:36:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+import "./app.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold underline font-add">Hello world!</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
